fb77ee4d8524f9a3e0dd7be517b8601ae1a14735
CSM <channaveeresh8197@gmail.com>
2024-06-30 02:02:15 +0530

    Update entities and repository tests

--- spring-data-jpa-project/src/main/java/com/channaveeresh/spring/data/pa/project/repository/CourseRepository.java
+++ spring-data-jpa-project/src/main/java/com/channaveeresh/spring/data/pa/project/repository/CourseRepository.java
@@ -1,10 +1,15 @@
 package com.channaveeresh.spring.data.pa.project.repository;
 
 import com.channaveeresh.spring.data.pa.project.entity.Course;
+import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
 
+
+    // findAll(Pageable pageable);
 }
--- spring-data-jpa-project/src/test/java/com/channaveeresh/spring/data/pa/project/repository/CourseRepositoryTest.java
+++ spring-data-jpa-project/src/test/java/com/channaveeresh/spring/data/pa/project/repository/CourseRepositoryTest.java
@@ -1,40 +1,67 @@
 package com.channaveeresh.spring.data.pa.project.repository;
 
+import com.channaveeresh.spring.data.pa.project.SpringDataJpaProjectApplication;
 import com.channaveeresh.spring.data.pa.project.entity.Course;
 import com.channaveeresh.spring.data.pa.project.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringDataJpaProjectApplication.class)
 class CourseRepositoryTest {
     @Autowired
     private CourseRepository courseRepository;
 
     @Test
-    public void saveRepository()
-    {
-        Course course=Course.builder().courseId(4L).credit(2).title("social").build();
-
-        courseRepository.save(course);
-    }
-    @Test
-    public void printAllCourses()
-    {
-        List<Course> s=courseRepository.findAll();
-        assertFalse(s.isEmpty());
-        System.out.println("s = " + s);
-    }
-    @Test
-    public void saveCourseWithTeacher()
-    {
-        Teacher teacher=Teacher.builder().firstName("narayan").LastName("hosuru").build();
-        Course course=Course.builder().teacher(teacher).title("Mathematics").credit(4).build();
+    public void saveRepository() {
+        Course course = Course.builder()
+                .courseId(4L)
+                .credit(2)
+                .title("social")
+                .build();
         courseRepository.save(course);
     }
 
+    @Test
+    public void printAllCourses() {
+        List<Course> courses = courseRepository.findAll();
+        assertFalse(courses.isEmpty());
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void saveCourseWithTeacher() {
+        Teacher teacher = Teacher.builder()
+                .firstName("narayan")
+                .LastName("hosuru")
+                .build();
+        Course course = Course.builder()
+                .teacher(teacher)
+                .title("Mathematics")
+                .credit(4)
+                .build();
+        courseRepository.save(course);
+    }
+
+    @Test
+    public void testFindCoursesByPage_ThirdPageWithTwoElements() {
+        int page = 2;
+        int pageSize = 1;
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<Course> courses = courseRepository.findAll(pageable);
+        int size=courses.getNumberOfElements();
+        List<Course> courseList = courses.getContent();
+
+        assertEquals(pageSize, courseList.size(), "Retrieved course list size should match page size");
+        System.out.println("courseList = " + courseList);
+        System.out.println("size->"+size);
+    }
 }
